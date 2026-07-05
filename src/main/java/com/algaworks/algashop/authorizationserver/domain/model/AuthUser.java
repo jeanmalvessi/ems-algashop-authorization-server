800baa10ee5df8750b04ea44bf8e8a475bbf7e06
Jean Malvessi <jeanmalvessi@gmail.com>
2026-07-05 14:55:40 -0300

    User persistence

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/domain/model/AuthUser.java
@@ -0,0 +1,30 @@
+package com.algaworks.algashop.authorizationserver.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "auth_user")
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuthUser extends AbstractAuditableAggregateRoot<AuthUser> {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String email;
+    private String password;
+    private String name;
+    private boolean enabled;
+
+    @Enumerated(EnumType.STRING)
+    private AuthUserType type;
+
+}
