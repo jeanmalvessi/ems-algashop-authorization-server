d5481e191ee81a68e956f07df4924fa37da953c8
Jean Malvessi <jeanmalvessi@gmail.com>
2026-07-05 15:32:23 -0300

    Customizing access token

--- src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/OIDCUserInfoService.java
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/OIDCUserInfoService.java
@@ -22,7 +22,7 @@ public class OIDCUserInfoService {
                 .name(authUser.getName())
                 .email(authUser.getEmail())
                 .claim("type", authUser.getType().name())
-                .claim("created_at", authUser.getCreatedAt().toEpochSecond())
+                .claim("created_at", String.valueOf(authUser.getCreatedAt().toEpochSecond()))
                 .build();
     }
 }
