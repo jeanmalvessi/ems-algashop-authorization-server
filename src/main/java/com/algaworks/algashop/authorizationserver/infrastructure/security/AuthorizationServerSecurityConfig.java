ad0f52c8f3e91fe7884f7b79fe13195b34086d1c
Jean Malvessi <frwk.jean@unimedpoa.com.br>
2026-07-07 13:36:14 -0300

    SecurityFilterChain + UserInfo + logout

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/security/AuthorizationServerSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.algaworks.algashop.authorizationserver.infrastructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class AuthorizationServerSecurityConfig {
+
+    private final OidcUserInfoMapper oidcUserInfoMapper;
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain authorizationServerFilterChain(HttpSecurity http) {
+        var authorizationServer = new OAuth2AuthorizationServerConfigurer();
+
+        http.securityMatcher(authorizationServer.getEndpointsMatcher())
+                .with(authorizationServer, configurer ->
+                        configurer.oidc(oidc -> oidc.userInfoEndpoint(
+                                userInfo -> userInfo.userInfoMapper(oidcUserInfoMapper))))
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .exceptionHandling(
+                        exceptions -> exceptions.defaultAuthenticationEntryPointFor(
+                                new LoginUrlAuthenticationEntryPoint("/login"),
+                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+}
